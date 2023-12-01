59e9a4960a145a9c2c98bef9d4c14745afd5c0cd
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-12-01 02:00:42 +0100

    docker init setup

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+
+async fn create_post() -> impl Responder {
+    HttpResponse::Created().body("Post created")
+}
+
+async fn read_posts() -> impl Responder {
+    HttpResponse::Ok().body("Here are all the posts")
+}
+
+async fn update_post() -> impl Responder {
+    HttpResponse::Ok().body("Post updated")
+}
+
+async fn delete_post() -> impl Responder {
+    HttpResponse::Ok().body("Post deleted")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/posts", web::post().to(create_post))
+            .route("/posts", web::get().to(read_posts))
+            .route("/posts/{id}", web::put().to(update_post))
+            .route("/posts/{id}", web::delete().to(delete_post))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
